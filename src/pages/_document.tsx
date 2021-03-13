c8229e859e6e43ff97db9775f18570bee8099773
edmilson-dk <droidvidaboa@gmail.com>
2021-03-12 21:36:38 -0300

    initial setup of project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
