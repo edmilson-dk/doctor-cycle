d79b3225e98e59dd8e79f650466a2877bd81cce5
edmilson-dk <droidvidaboa@gmail.com>
2021-03-16 16:47:07 -0300

    feat: finished header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,57 @@
+import { FiPhoneCall, FiMapPin, FiClock } from 'react-icons/fi';
+
 import Container from "src/styles/components/Container";
 
 import {
   HeaderWrapper,
+  HeaderContainer,
   HeaderLogo,
- } from 'src/styles/pages/home';
+  HeaderMenu,
+  HeaderMenuItem,
+  HeaderMenuItemIcon,
+  HeaderMenuItemContent
+ } from 'src/styles/components/Header';
  
 function Header() {
   return (
     <HeaderWrapper>
       <Container>
-        <HeaderLogo>
-          <img src="assets/icons/logo.svg" />
-        </HeaderLogo>
+        <HeaderContainer>
+          <HeaderLogo>
+            <img src="assets/icons/logo.svg" />
+          </HeaderLogo>
+          <HeaderMenu>
+            <HeaderMenuItem>
+              <HeaderMenuItemIcon>
+                <FiPhoneCall size="100%" />
+              </HeaderMenuItemIcon>
+              <HeaderMenuItemContent>
+                <strong>Emergency</strong>
+                <p>0800-999-000</p>
+              </HeaderMenuItemContent>
+            </HeaderMenuItem>
+
+            <HeaderMenuItem>
+              <HeaderMenuItemIcon>
+                <FiClock size="100%" />
+              </HeaderMenuItemIcon>
+              <HeaderMenuItemContent>
+                <strong>Working Time</strong>
+                <p>09:00-20:00 every day</p>
+              </HeaderMenuItemContent>
+            </HeaderMenuItem>
+
+            <HeaderMenuItem>
+              <HeaderMenuItemIcon>
+                <FiMapPin size="100%" />
+              </HeaderMenuItemIcon>
+              <HeaderMenuItemContent>
+                <strong>Location</strong>
+                <p>New Work Av190</p>
+              </HeaderMenuItemContent>
+            </HeaderMenuItem>
+          </HeaderMenu>
+        </HeaderContainer>
       </Container>
     </HeaderWrapper>
   )
