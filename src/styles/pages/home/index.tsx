9dbfa6785b274cc0dff44b93fb26d60c2af365b2
edmilson-dk <droidvidaboa@gmail.com>
2021-03-16 15:32:55 -0300

    feat: created pages folder styles

---
+++ src/styles/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+`;
+
+export const HeaderLogo = styled.div`
+  width: 100%;
+`;
+
+export const Title = styled.h1`
+  color: var(--blue-primary);
+  font-size: 4rem;  
+`;
