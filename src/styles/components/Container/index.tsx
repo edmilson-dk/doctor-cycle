ad394aa76227ff8f12e017aa7dbb8611289fda1f
edmilson-dk <droidvidaboa@gmail.com>
2021-03-16 15:31:18 -0300

    feat: created files of header component styles

---
+++ src/styles/components/Container/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 390px;
+  margin: 0 auto;
+  padding: 0 10px;
+`;
+
+export default Container;
