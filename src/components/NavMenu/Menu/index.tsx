944d81eeb431d0f97141670110537c53af9602ba
edmilson-dk <droidvidaboa@gmail.com>
2021-03-18 21:27:55 -0300

    feat: added nav menu context

--- src/components/NavMenu/Menu/index.tsx
+++ src/components/NavMenu/Menu/index.tsx
@@ -1,80 +1,104 @@
-import { FiCheckSquare, FiSearch } from "react-icons/fi";
+import { useContext, useState } from 'react';
+import { FiCheckSquare, FiSearch, FiArrowRight } from 'react-icons/fi';
 
-import Container from "src/styles/components/Container";
+import { NavMenuContext } from 'src/contexts/NavMenuContext';
+import Container from 'src/styles/components/Container';
 
 import {
   MenuWrapper,
   MenuContainer,
   MenuList,
   MenuInputSearch,
-  MenuListItem,
-  MenuShadow
-} from "src/styles/components/NavMenu/Menu";
+  MenuListItem
+} from 'src/styles/components/NavMenu/Menu';
 
 function Menu() {
+  const [ isActiveLink, setIsActiveLink ] = useState(false);
+  const { isOpenMenu } = useContext(NavMenuContext);
+
   return (
-    <MenuWrapper>
+    <MenuWrapper isOpen={isOpenMenu}>
       <Container>
         <MenuContainer>
           <MenuList>
-            <MenuInputSearch>
-              <input
-               type="text" 
-               name="search" 
-               placeholder="Search services" />
-               <button>
-                 <span>
-                   <FiSearch size="100%" />
-                 </span>
-               </button>
-            </MenuInputSearch>
+            <ul>
+              <MenuInputSearch>
+                <input
+                type='text' 
+                name='search' 
+                placeholder='Search services' />
+                <button>
+                  <span>
+                    <FiSearch size='100%' />
+                  </span>
+                </button>
+              </MenuInputSearch>
 
-            <MenuListItem isActive={true}>
-              <a href="#home">Home</a>
-              <span>
-               <FiCheckSquare size="100%"/> 
-              </span>
-            </MenuListItem>
+              <MenuListItem isActive={true}>
+                <a href='#home'>Home</a>
+                <span>
+                  { true
+                      ? <FiCheckSquare size='100%'/>
+                      : <FiArrowRight size='100%'/>
+                  }
+                </span>
+              </MenuListItem>
 
-            <MenuListItem isActive={false}>
-              <a href="#about">About us</a>
-              <span>
-               <FiCheckSquare size="100%"/> 
-              </span>
-            </MenuListItem>
+              <MenuListItem isActive={isActiveLink}>
+                <a href='#about'>About us</a>
+                <span>
+                  { isActiveLink 
+                      ? <FiCheckSquare size='100%'/>
+                      : <FiArrowRight size='100%'/>
+                  }
+                </span>
+              </MenuListItem>
 
-            <MenuListItem isActive={false}>
-              <a href="#services">Services</a>
-              <span>
-               <FiCheckSquare size="100%"/> 
-              </span>
-            </MenuListItem>
+              <MenuListItem isActive={isActiveLink}>
+                <a href='#services'>Services</a>
+                <span>
+                  { isActiveLink 
+                      ? <FiCheckSquare size='100%'/>
+                      : <FiArrowRight size='100%'/>
+                  }
+                </span>
+              </MenuListItem>
 
-            <MenuListItem isActive={true}>
-              <a href="#doctors">Doctors</a>
-              <span>
-               <FiCheckSquare size="100%"/> 
-              </span>
-            </MenuListItem>
+              <MenuListItem isActive={isActiveLink}>
+                <a href='#doctors'>Doctors</a>
+                <span>
+                  { isActiveLink 
+                      ? <FiCheckSquare size='100%'/>
+                      : <FiArrowRight size='100%'/>
+                  }
+                </span>
+              </MenuListItem>
 
-            <MenuListItem isActive={false}>
-              <a href="#news">News</a>
-              <span>
-               <FiCheckSquare size="100%"/> 
-              </span>
-            </MenuListItem>
+              <MenuListItem isActive={isActiveLink}>
+                <a href='#news'>News</a>
+                <span>
+                  { isActiveLink 
+                      ? <FiCheckSquare size='100%'/>
+                      : <FiArrowRight size='100%'/>
+                  }
+                </span>
+              </MenuListItem>
 
-            <MenuListItem isActive={false}>
-              <a href="#contact">Contact</a>
-              <span>
-               <FiCheckSquare size="100%"/> 
-              </span>
-            </MenuListItem>
+              <MenuListItem isActive={isActiveLink}>
+                <a href='#contact'>Contact</a>
+                <span>
+                  { isActiveLink 
+                      ? <FiCheckSquare size='100%'/>
+                      : <FiArrowRight size='100%'/>
+                  }
+                </span>
+              </MenuListItem>
+            </ul>
           </MenuList>
-
-          <MenuShadow />
         </MenuContainer>
       </Container>
     </MenuWrapper>
   )
 }
+
+export default Menu;
