8000e5256c321df2494b4ec388c0599747dc0d00
edmilson-dk <droidvidaboa@gmail.com>
2021-03-18 21:28:28 -0300

    feat: added nav menu context provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Header from "src/components/Header";
 import NavMenu from "src/components/NavMenu";
+import { NavMenuContextProvider } from "src/contexts/NavMenuContext";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <NavMenu />
+      <NavMenuContextProvider>
+        <NavMenu />
+      </NavMenuContextProvider>
     </>
   );
 }
