a0fb696224fe7e991812a16eba20c428c169b314
edmilson-dk <droidvidaboa@gmail.com>
2021-03-18 21:28:05 -0300

    feat: added nav menu context

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -1,15 +1,21 @@
-import { FiAlignCenter } from "react-icons/fi"
+import { useContext } from 'react';
+import { FiAlignCenter } from 'react-icons/fi'
+import { NavMenuContext } from 'src/contexts/NavMenuContext';
 
-import Container from "src/styles/components/Container";
+import Container from 'src/styles/components/Container';
 
 import {
   NavMenuWrapper,
   NavMenuContainer,
   NavMenuAppointmentButton,
   NavMenuButtonMenu
-} from "src/styles/components/NavMenu";
+} from 'src/styles/components/NavMenu';
+
+import Menu from './Menu';
 
 function NavMenu() {
+  const { setOpenMenu } = useContext(NavMenuContext);
+
   return (
     <NavMenuWrapper>
       <Container>
@@ -17,13 +23,14 @@ function NavMenu() {
           <NavMenuAppointmentButton>
             Appointment
           </NavMenuAppointmentButton>
-          <NavMenuButtonMenu>
+          <NavMenuButtonMenu onClick={setOpenMenu}>
             <span>
-              <FiAlignCenter size="100%"/>
+              <FiAlignCenter size='100%'/>
             </span>
           </NavMenuButtonMenu>
         </NavMenuContainer>
       </Container>
+      <Menu />
     </NavMenuWrapper>
   );
 }
