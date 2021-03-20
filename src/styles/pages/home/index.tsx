1854773f8f92ea598175487a5e4871ff93a448e3
edmilson-dk <droidvidaboa@gmail.com>
2021-03-20 15:17:29 -0300

    feat: created home wrapper styles

--- src/styles/pages/home/index.tsx
+++ src/styles/pages/home/index.tsx
@@ -1,14 +1,6 @@
 import styled from "styled-components";
 
-export const HeaderWrapper = styled.header`
+export const HomeWrapper = styled.header`
   width: 100%;
-`;
-
-export const HeaderLogo = styled.div`
-  width: 100%;
-`;
-
-export const Title = styled.h1`
-  color: var(--blue-primary);
-  font-size: 4rem;  
+  overflow: hidden;
 `;
