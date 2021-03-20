26f387b33b32d6e13483e259c98bd0505054868b
edmilson-dk <droidvidaboa@gmail.com>
2021-03-20 15:18:46 -0300

    feat: created title and sub title styles components

---
+++ src/styles/components/TitleAndSubTitle/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-size: var(--title-primary);
+  font-weight: var(--bold);
+  text-align: center;
+  margin-bottom: var(--space-4);
+  color: var(--blue-dark);
+`;
+
+export const SubTitle = styled.h3`
+  font-size: var(--sub-title);
+  font-weight: var(--medium);
+  text-align: center;
+  margin-bottom: var(--space-2);
+  color: var(--gray);
+`;
