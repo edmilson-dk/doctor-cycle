876ba45364142d964078cd782c3df345d2eae4ed
edmilson-dk <droidvidaboa@gmail.com>
2021-03-23 13:06:21 -0300

    refactor: removed viewport tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;600;700;800&display=swap" rel="stylesheet" />
         </Head>
